2dcda4c325f56fd8894313bb633b80ed44d5a706
RyderChris <ryderc@oscoit.com>
2026-03-13 14:47:57 -0300

    Ryder chris patch 4 (#6)
    * Add library reference for platform/amba
    
    * Add 'amba_root' to management groups in YAML
    
    * Restore built-in replacements and custom settings
    
    * Refactor management_resources module and add amba module
    
    * Restore comments and structure in auto.tfvars
    
    * Restore comments and structure in auto.tfvars
    
    Restored comments and structure in platform-landing-zone.auto.tfvars file.
    
    * Update terraform.tf
    
    * Fix formatting in terraform.tf for provider alz

--- terraform.tf
+++ terraform.tf
@@ -25,6 +25,10 @@ provider "alz" {
   library_overwrite_enabled = true
   library_references = [
     {
+      path = "platform/amba"
+      ref  = "2026.01.1"
+    },
+    {
       custom_url = "${path.root}/lib"
     }
   ]
